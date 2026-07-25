04bf227c4ebe8e4a1cc607e11c9a8705b684eff7
Username772 <103868024+Username772@users.noreply.github.com>
2026-07-25 15:20:33 -0400

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -1,1 +1,14 @@
+# scripts/train.py
 
+from src.data import load_data, split_data
+from src.model import train_logistic_regression
+
+
+df = load_data("data/triage_cleaned_v1.csv")
+
+X_train, X_test, y_train, y_test = split_data(df)
+
+model = train_logistic_regression(
+    X_train,
+    y_train
+)
