da1f72f89f44e812e84f90bad137aa2b5cceb1d2
Username772 <103868024+Username772@users.noreply.github.com>
2026-07-25 16:10:18 -0400

    Update data.py

--- src/data.py
+++ src/data.py
@@ -1,21 +1,18 @@
-# src/data.py
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
 def load_data(filepath):
     """
-    Load cleaned triage dataset.
+    Load the cleaned ED triage dataset.
     """
     return pd.read_csv(filepath)
 
 
 def split_data(df, target="esi", test_size=0.20, random_state=42):
     """
-    Split dataset into training and testing sets.
+    Split the dataset into training and testing sets.
     """
-
     X = df.drop(columns=[target])
     y = df[target]
 
