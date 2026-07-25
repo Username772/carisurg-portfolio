68ef4d747e00def476a53510f756b72a7297fe09
Username772 <103868024+Username772@users.noreply.github.com>
2026-07-25 15:19:09 -0400

    Update data.py

--- src/data.py
+++ src/data.py
@@ -1,6 +1,30 @@
-from src.data import load_data, split_data
-from src.model import train_logistic_regression
+# src/data.py
 
-df = load_data("data/triage_cleaned_v1.csv")
-X_train, X_test, y_train, y_test = split_data(df)
-model = train_logistic_regression(X_train, y_train)
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def load_data(filepath):
+    """
+    Load cleaned triage dataset.
+    """
+    return pd.read_csv(filepath)
+
+
+def split_data(df, target="esi", test_size=0.20, random_state=42):
+    """
+    Split dataset into training and testing sets.
+    """
+
+    X = df.drop(columns=[target])
+    y = df[target]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=test_size,
+        stratify=y,
+        random_state=random_state
+    )
+
+    return X_train, X_test, y_train, y_test
